acae03f4802a68ab8513ee3e2f82290ddd1f08dd
Marcelo Oliveira <capyvara@gmail.com>
2019-08-01 17:58:20 -0300

    Improved wrapper

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar -cvzf unityyamlmerge.tar.gz UnityYAMLMerge mergerules.txt mergespecfile.txt uyaml
+tar -cvzf unityyamlmerge.tar.gz UnityYAMLMerge mergerules.txt mergespecfile.txt mergeresolving.txt uyaml
 shasum -a 256 unityyamlmerge.tar.gz
